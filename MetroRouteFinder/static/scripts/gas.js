ea5a7bcaf06b593cddb525610ae05c2f3833f81d
Jason Lee <a959859jason@gmail.com>
2016-07-18 12:29:26 +0800

    Minimialize

---
+++ MetroRouteFinder/static/scripts/gas.js
@@ -0,0 +1,16 @@
+(function(i, s, o, g, r, a, m) {
+    i["GoogleAnalyticsObject"] = r;
+    i[r] = i[r] || function() {
+        (i[r].q = i[r].q || []).push(arguments);
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m);
+})(window, document, "script", "https://www.google-analytics.com/analytics.js", "ga");
+
+ga("create", "UA-7969851-10", "auto");
+
+ga("send", "pageview");
+
+var GaodeKey = "e5035e84785eb4da3362836ee5f92cf8", DisplayKey = "f9c83cfb12c018f512d8a97878b8c580";
