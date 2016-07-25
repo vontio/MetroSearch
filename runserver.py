ec29f38d29a14ec8782b5d78949c25f6b0105d5c
Jason Lee <a959859jason@gmail.com>
2016-07-25 15:15:15 +0800

    Remove Duplicate

--- runserver.py
+++ runserver.py
@@ -1,19 +1,17 @@
+# -*- coding: utf-8 -*-
 """
 This script runs the Management application using a development server.
 """
 
 from os import environ
+import webbrowser
 from MetroRouteFinder import app
 
 if __name__ == '__main__':
     DEBUG = environ.get("SVR_DEBUG", False)
     HOST = environ.get('SVR_HOST', '0.0.0.0')
-    # try:
-    #     PORT = int(environ.get('SVR_PORT', '5555'))
-    # except ValueError:
-    #     PORT = 5555
     PORT = 63876
     DEBUG = True
-    context = ("C:\users\jason\JasonLee.test.crt",
-               "C:\users\jason\JasonLee.test.key")
+    context = ("C:\users\jason\JasonLee.test.crt", "C:\users\jason\JasonLee.test.key")
+    webbrowser.open("https://localhost:{p}".format(p=PORT), new=0, autoraise=True)
     app.run(HOST, PORT, debug=DEBUG, ssl_context=context)
